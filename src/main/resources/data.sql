1a7fc951d926c28bc33b67b581fc3cf54d0ba9c7
sejin park <seijin0722@naver.com>
2023-10-25 20:14:49 +0900

    feat : Spring 서버 실행시 insert문 쿼리 자동 실행

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,22 @@
-INSERT INTO Claim (claim_status, claim_type, created_at)
-VALUES ('INCOMING', 'WRONG_DELIVERY_LOCATION', NOW(6)),
-       ('INCOMING', 'DELAYED_DELIVERY', NOW(6)),
-       ('INCOMING', 'MISSING_ITEM', NOW(6)),
-       ('INCOMING', 'DAMAGED_ITEM', NOW(6)),
-       ('INCOMING', 'WRONG_DELIVERY_LOCATION', NOW(6)),
-       ('INCOMING', 'DELAYED_DELIVERY', NOW(6)),
-       ('INCOMING', 'MISSING_ITEM' ,NOW(6)),
-       ('INCOMING','DAMAGED_ITEM' ,NOW(6)),
-       ('INCOMING','WRONG_DELIVERY_LOCATION' ,NOW(6)),
-       ('INCOMING','DELAYED_DELIVERY' ,NOW(6));
+-- sql
+INSERT INTO carrier (carrier_name)
+VALUES ('CJ_LOGISTICS'),
+       ('LOTTE_LOGISTICS'),
+       ('HYUNDAI_LOGISTICS'),
+       ('DHL'),
+       ('KOREA_POST');
+INSERT INTO customer (contact_number, contact_person, name)
+VALUES ('John Doe','123-456-7890','ABC Company'),
+       ('John','123-456-7890','choco Company'),
+       ('Doe','123-456-7890','teamteam'),
+       ('park','123-456-7890','rocket fresh'),
+       ('seijin','123-456-7890','coupang');
+INSERT INTO voc
+(blame_type, voc_content, created_at, updated_at, claim_entry_type, claim_status, customer_id, carrier_id)
+VALUES ('CARRIER', 'WRONG_DELIVERY_LOCATION', CURRENT_TIMESTAMP(6),CURRENT_TIMESTAMP(6),'KAKAOTALK','INCOMING',1,1),
+       ('CUSTOMER', 'DAMAGED_ITEM', CURRENT_TIMESTAMP(6),NOW(6),'NAVER_TALK','INCOMING',1,2),
+       ('CARRIER', 'DELAYED_DELIVERY', CURRENT_TIMESTAMP(6),CURRENT_TIMESTAMP(6),'PHONE','INCOMING',2,3),
+       ('CUSTOMER', 'MISSING_ITEM', CURRENT_TIMESTAMP(6),NOW(6),'KAKAOTALK','INCOMING',2,4),
+       ('CARRIER', 'DELAYED_DELIVERY', CURRENT_TIMESTAMP(6),CURRENT_TIMESTAMP(6),'PHONE','INCOMING',3,5);
+
+
